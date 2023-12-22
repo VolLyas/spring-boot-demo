2e6cd3b5990a6e6fd74e0b3b23a9f8534324eafc
voval <vova.lyashik@gmail.com>
2023-12-22 10:20:22 +0200

    Added logger

--- src/main/java/com/voldlyas/demo/DemoProjApplication.java
+++ src/main/java/com/voldlyas/demo/DemoProjApplication.java
@@ -1,13 +1,16 @@
 package com.voldlyas.demo;
 
+import lombok.extern.java.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Log
 @SpringBootApplication
 public class DemoProjApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoProjApplication.class, args);
+		log.warning("Hello in Spring boot!");
 	}
 
 }
